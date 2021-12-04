4bc8c33d6710ca889ed2dcae04fa79d0bde649d4
Henrik Bierbum Bacher <Bacher@gmail.com>
2021-12-04 09:22:52 +0100

    Add day 3 solution
    sample problems works, not full solution

---
+++ 2021/Day 03/Day 03/Program.cs
@@ -0,0 +1,39 @@
+﻿List<string> diagnoticsReports = File.ReadAllLines("data.txt").Select(x => x.Substring(0,5)).ToList(); ;
+
+Dictionary<int, int> occurancesOfOne = new Dictionary<int, int>();
+
+foreach (var report in diagnoticsReports)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (report[i] == '1')
+        {
+            if (occurancesOfOne.ContainsKey(i))
+            {
+                occurancesOfOne[i]++;
+            }
+            else
+            {
+                occurancesOfOne.Add(i, 1);
+            }
+        }
+    }
+}
+
+string gammaRate = string.Empty;
+string epsilonRate = string.Empty;
+for (int i = 0; i < 5; i++)
+{
+    if (occurancesOfOne[i] >= diagnoticsReports.Count/2)
+    {
+        gammaRate += "1";
+        epsilonRate += "0";
+    }
+    else
+    {
+        gammaRate += "0";
+        epsilonRate += "1";
+    }
+}
+
+Console.WriteLine(String.Format("Part 1 solution: {0}", Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2)));
