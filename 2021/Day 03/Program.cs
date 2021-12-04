fc219c4acb6c9d7c0a9007e895112097979900e1
Henrik Bierbum Bacher <Bacher@gmail.com>
2021-12-04 16:55:30 +0100

    Fix day 3 part 1

--- 2021/Day 03/Program.cs
+++ 2021/Day 03/Program.cs
@@ -1,10 +1,10 @@
-﻿List<string> diagnoticsReports = File.ReadAllLines("data.txt").Select(x => x[..5]).ToList(); ;
+﻿List<string> diagnoticsReports = File.ReadAllLines("data.txt").ToList(); ;
 
 Dictionary<int, int> occurancesOfOne = new();
 
 foreach (var report in diagnoticsReports)
 {
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < report.Length; i++)
     {
         if (report[i] == '1')
         {
@@ -23,7 +23,7 @@ foreach (var report in diagnoticsReports)
 string gammaRate = string.Empty;
 string epsilonRate = string.Empty;
 
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < diagnoticsReports.First().Length; i++)
 {
     if (occurancesOfOne[i] >= diagnoticsReports.Count/2)
     {
@@ -37,4 +37,23 @@ for (int i = 0; i < 5; i++)
     }
 }
 
-Console.WriteLine(String.Format("Part 1 solution: {0}", Convert.ToInt64(gammaRate, 2) * Convert.ToInt64(epsilonRate, 2)));
+Console.WriteLine(String.Format("Part 1 solution: {0}", (Convert.ToInt64(gammaRate, 2) * Convert.ToInt64(epsilonRate, 2))));
+
+
+
+for (int i = 0; i < diagnoticsReports.Count; i++)
+{
+    for (int j = 0; j < diagnoticsReports[i].Length; j++)
+    {
+        if (diagnoticsReports[i][j] == gammaRate[j])
+        {
+            continue;
+        }
+        else
+        {
+            break;
+        }
+        
+    }
+    Console.WriteLine(diagnoticsReports[i]);
+}
